fe0d21ca268d09283699756cff6ce6b3a2931a05
Buzzinga App <sujith@buzzinga.com>
2015-08-01 20:00:05 +0530

    Initial Commit

---
+++ app/src/main/java/com/headrun/buzzinga/config/Constants.java
@@ -0,0 +1,20 @@
+package com.headrun.buzzinga.config;
+
+import com.headrun.buzzinga.doto.SearchDetails;
+
+import java.util.ArrayList;
+
+/**
+ * Created by headrun on 30/7/15.
+ */
+public class Constants {
+
+    public static String scroolid="1";
+    public static String FIRSTURL="";
+    public static boolean swipedata=false;
+    public static boolean finddata=false;
+    public static ArrayList<SearchDetails> listdetails=new ArrayList<SearchDetails>();
+
+}
+
+
---
+++ app/src/main/java/com/headrun/buzzinga/utils/ConnectionSettings.java
@@ -0,0 +1,29 @@
+package com.headrun.buzzinga.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+/**
+ * Created by headrun on 20/7/15.
+ */
+public class ConnectionSettings {
+
+    public static boolean isConnected(Context context){
+        ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null)
+        {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED)
+                    {
+                        Log.i("Log_Tag","conneced network is"+info[i]+"sate"+info[i].getState());
+                        return true;
+                    }
+
+        }
+        return false;
+    }
+}
